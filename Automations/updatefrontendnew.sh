ffd618b79fdc579c35181352aa5df4af9a789843
ShwetaMG <89741655+ShwetaMG@users.noreply.github.com>
2026-07-05 19:46:37 +0530

    Changed INSTANCE_ID for EC2 instance
    Updated the INSTANCE_ID to a new EC2 instance.

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-04926f26d5079cee1"
+INSTANCE_ID="i-0e1bf1b2b4bbc80d1"
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
